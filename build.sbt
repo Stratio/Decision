2388a17ab3ef580959504d29eca462068e9a7255
Alberto <albertorodriguez@paradigmatecnologico.com>
2014-03-20 11:04:51 +0100

    Added logging features

--- build.sbt
+++ build.sbt
@@ -29,7 +29,8 @@ libraryDependencies ++= Seq(
   "org.scalatest" % "scalatest_2.10" % "2.0" % "test" withSources() withJavadoc(),
   "org.apache.kafka" % "kafka_2.10" % "0.8.0" exclude("com.sun.jdmk", "jmxtools") exclude("com.sun.jmx", "jmxri"),
   "com.typesafe" % "config" % "1.2.0",
-  "org.apache.curator" % "curator-framework" % "2.4.1"
+  "org.apache.curator" % "curator-framework" % "2.4.1",
+  "org.slf4j" % "slf4j-log4j12" % "1.7.5"
 )
 
 initialCommands := "import com.stratio.bus._"
--- src/main/scala/com/stratio/bus/BusSyncOperation.scala
+++ src/main/scala/com/stratio/bus/BusSyncOperation.scala
@@ -6,40 +6,41 @@ import scala.concurrent.duration._
 import java.util.UUID
 import com.stratio.bus.utils.JsonUtils
 import com.stratio.bus.StreamingAckValues._
+import org.slf4j.LoggerFactory
 
 case class BusSyncOperation(
   tableProducer: KafkaProducer,
   zookeeperConsumer: ZookeeperConsumer,
   operation: String) {
   val config = ConfigFactory.load()
+  val log = LoggerFactory.getLogger(getClass)
   val streamingAckTimeOut = config.getString("streaming.ack.timeout.in.seconds").toInt
 
   def performSyncOperation(queryString: String) = {
     val zNodeUniqueId = UUID.randomUUID().toString
     addMessageToKafkaTopic(queryString, zNodeUniqueId)
-    waitForTheStreamingResponse(zNodeUniqueId)
+    waitForTheStreamingResponse(zNodeUniqueId, queryString)
   }
 
   private def addMessageToKafkaTopic(queryString: String, creationUniqueId: String) = {
     val message = JsonUtils.appendElementsToJsonString(queryString, Map("zNodeId" -> creationUniqueId))
     tableProducer.send(message, operation)
   }
-  private def waitForTheStreamingResponse(zNodeUniqueId: String) = {
+  private def waitForTheStreamingResponse(zNodeUniqueId: String, queryString: String) = {
+    val zNodeFullPath = getOperationZNodeFullPath(zNodeUniqueId, operation)
     try {
-      val zNodeFullPath = getOperationZNodeFullPath(zNodeUniqueId, operation)
       Await.result(zookeeperConsumer.readZNode(zNodeFullPath), streamingAckTimeOut seconds)
       val response = zookeeperConsumer.getZNodeData(zNodeFullPath)
-      println("The response has been: "+response.get)
       response.get match {
-        case StreamingAckValues(AckOk) => println("ACK OK!!")
-        case StreamingAckValues(AckError) => println("ACK ERROR!!!")
-        case _ => println("I HAVE NO IDEA WHAT TO DO WITH THIS :(")
+        case StreamingAckValues(AckOk) => log.info("Stratio Bus - Ack received for: "+queryString)
+        case StreamingAckValues(AckError) => log.error("Stratio Bus - Ack error [ACK_CODE,QUERY_STRING]: ["+AckError+","+queryString+"]")
+        case _ => log.info("Stratio Bus - I have no idea what to do with this")
       }
       //TODO define response (json, exceptions....)
       zookeeperConsumer.removeZNode(zNodeFullPath)
     } catch {
       case e: TimeoutException => {
-        println("TIME OUT")
+        log.error("Stratio Bus - Ack from zookeeper timeout expired for: "+queryString)
         //TODO insert error into error-topic ???
       }
     }
