25bc697cdb912e7144700e6f22f441af05b569bc
Alberto <albertorodriguez@paradigmatecnologico.com>
2014-04-01 12:21:37 +0200

    Added generic exception constructor

--- src/main/java/com/stratio/streaming/commons/exceptions/StratioStreamingException.java
+++ src/main/java/com/stratio/streaming/commons/exceptions/StratioStreamingException.java
@@ -2,7 +2,12 @@ package com.stratio.streaming.commons.exceptions;
 
 public class StratioStreamingException extends Exception {
 
-    private String errorCode="Stratio_Streaming_Exception";
+    private String errorCode="Stratio Streaming Exception";
+
+    public StratioStreamingException(String message){
+        super(message);
+        this.errorCode=errorCode;
+    }
 
     public StratioStreamingException(String message, String errorCode){
         super(message);
