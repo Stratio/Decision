7782ed0d3f9b9c48f077fa851766f1153ce5a5df
Alberto <albertorodriguez@paradigmatecnologico.com>
2014-04-02 18:12:26 +0200

    StreamParser unit tests

---
+++ src/main/scala/com/stratio/bus/utils/StreamsParser.scala
@@ -0,0 +1,21 @@
+package com.stratio.bus.utils
+
+import com.google.gson.Gson
+import com.stratio.streaming.commons.messages.ListStreamsMessage
+import com.stratio.streaming.commons.streams.StratioStream
+import scala.collection.JavaConversions._
+
+object StreamsParser {
+
+  def parse(json: String) = {
+    val listStreams = new Gson().fromJson(json, classOf[ListStreamsMessage]).getStreams.toList
+    val stratioStreams = listStreams.map(stream => {
+      new StratioStream(stream.getStreamName,
+        stream.getColumns,
+        stream.getQueries,
+        stream.getUserDefined)}
+    )
+    stratioStreams
+  }
+
+}
