aededb7d407a9ce49a560005ef588556a04f3d5e
DAVID MORALES <dmorales@paradigmatecnologico.com>
2014-04-02 13:02:36 +0200

    -

--- src/main/java/com/stratio/streaming/commons/constants/BUS.java
+++ src/main/java/com/stratio/streaming/commons/constants/BUS.java
@@ -1,7 +1,7 @@
 package com.stratio.streaming.commons.constants;
 
 public interface BUS {
-	public static final String STREAMING_GROUP_ID 	= "stratioStreaming";
+	 public static final String STREAMING_GROUP_ID 	= "stratioStreaming";
 	public static final String TOPICS				= "stratio_streaming_requests";
 	public static final String LIST_STREAMS_TOPIC	= "stratio_streaming_list_streams";
 //	
