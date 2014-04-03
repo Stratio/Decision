bea8a7ffab205f0a0fc50f2a7fb345b3d99c6660
Alberto <albertorodriguez@paradigmatecnologico.com>
2014-04-03 11:22:11 +0200

    Added COLUMN_TYPES

---
+++ src/main/java/com/stratio/streaming/commons/constants/COLUMN_TYPES.java
@@ -0,0 +1,10 @@
+package com.stratio.streaming.commons.constants;
+
+public class COLUMN_TYPES {
+    public static final String STRING = "STRING";
+    public static final String BOOLEAN = "BOOLEAN";
+    public static final String DOUBLE = "DOUBLE";
+    public static final String INTEGER = "INTEGER";
+    public static final String LONG = "LONG";
+    public static final String FLOAT = "FLOAT";
+}
