ed811fd0cd8e6b484a02e6e1478d83639f2c6c13
DAVID MORALES <dmorales@paradigmatecnologico.com>
2014-04-21 15:07:24 +0200

    ADD SAVE2CASSANDRA TOPIC

--- src/main/java/com/stratio/streaming/commons/constants/STREAMING.java
+++ src/main/java/com/stratio/streaming/commons/constants/STREAMING.java
@@ -1,10 +1,11 @@
 package com.stratio.streaming.commons.constants;
 
 public interface STREAMING {
-	public static final int    DURATION_MS				= 2000;
-	public static final String ZK_BASE_PATH				= "/stratio/streaming";
-	public static final String STREAM_STATUS_MAP 		= "stratio_stream_map";
-	public static final String INTERNAL_LISTEN_TOPIC 	= "stratio_listen"; 
+	public static final int    DURATION_MS						= 2000;
+	public static final String ZK_BASE_PATH						= "/stratio/streaming";
+	public static final String STREAM_STATUS_MAP 				= "stratio_stream_map";
+	public static final String INTERNAL_LISTEN_TOPIC 			= "stratio_listen";
+	public static final String INTERNAL_SAVE2CASSANDRA_TOPIC 	= "stratio_save2cassandra";
 	public static final String ZK_EPHEMERAL_NODE_PATH			= "/stratio/streaming/engine";
 
 
