bf7ce7d6180ee290a619c73b2c05d69b49ababdb
Alberto Rodriguez <albertorodriguez@paradigmatecnologico.com>
2014-05-21 11:36:13 +0200

    Increase the ack time-out

--- src/main/scala/com/stratio/streaming/api/StratioStreamingAPIConfig.scala
+++ src/main/scala/com/stratio/streaming/api/StratioStreamingAPIConfig.scala
@@ -20,5 +20,5 @@ import com.typesafe.config.ConfigFactory
 
 class StratioStreamingAPIConfig {
   val config = ConfigFactory.load("stratio-streaming.conf")
-  val streamingAckTimeOutInSeconds = 4
+  val streamingAckTimeOutInSeconds = 8
 }
