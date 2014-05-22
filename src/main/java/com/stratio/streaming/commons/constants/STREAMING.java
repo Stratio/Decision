5b596e5ea2794d09a064230e1f1b65560100d8c0
Antonio Jesus Navarro Perez <ajnavarro@paradigmatecnologico.com>
2014-05-22 10:12:57 +0200

    deleted duration property

--- src/main/java/com/stratio/streaming/commons/constants/STREAMING.java
+++ src/main/java/com/stratio/streaming/commons/constants/STREAMING.java
@@ -16,7 +16,6 @@
 package com.stratio.streaming.commons.constants;
 
 public interface STREAMING {
-    public static final int DURATION_MS = 7000;
     public static final String ZK_BASE_PATH = "/stratio/streaming";
     public static final String STREAM_STATUS_MAP = "stratio_stream_map";
     public static final String INTERNAL_LISTEN_TOPIC = "stratio_listen";
