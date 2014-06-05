d8dd6180f5e6c50cbbd3ffbc99737e548a9bca4f
Antonio Jesus Navarro Perez <ajnavarro@paradigmatecnologico.com>
2014-06-05 18:55:16 +0200

    added new reply code

--- commons/src/main/java/com/stratio/streaming/commons/constants/REPLY_CODES.java
+++ commons/src/main/java/com/stratio/streaming/commons/constants/REPLY_CODES.java
@@ -34,6 +34,7 @@ public abstract class REPLY_CODES {
     public static final Integer KO_SOURCE_STREAM_DOES_NOT_EXIST = 15;
     public static final Integer KO_INDEX_STREAM_ALREADY_ENABLED = 16;
     public static final Integer KO_STREAM_OPERATION_NOT_ALLOWED = 17;
+    public static final Integer KO_SAVE2MONGO_STREAM_ALREADY_ENABLED = 18;
 
     public static String getReadableErrorFromCode(Integer code) {
 
@@ -90,6 +91,9 @@ public abstract class REPLY_CODES {
             break;
         case 17:
             decodedReply = "KO: STREAM OPERATION NOT ALLOWED";
+            break;
+        case 18:
+            decodedReply = "KO: SAVE2MONGO IN THIS STREAM IS ALREADY_ENABLED";
             break;
         default:
             decodedReply = "UNKOWN ERROR";
