9a7dbf46c110dc412c54e8030a777563fc1e3b30
Alberto Rodriguez <albertorodriguez@paradigmatecnologico.com>
2014-06-11 15:18:04 +0200

    Zookeeper test with embedded server.

--- api/src/test/scala/com/stratio/streaming/unit/ZookeeperConsumerUnitTests.scala
+++ api/src/test/scala/com/stratio/streaming/unit/ZookeeperConsumerUnitTests.scala
@@ -20,7 +20,8 @@ import scala.concurrent._
 import scala.concurrent.duration._
 import java.util.UUID
 import com.stratio.streaming.zookeeper.ZookeeperConsumer
-import org.apache.curator.retry.ExponentialBackoffRetry
+import org.apache.curator.retry.RetryOneTime
+import com.netflix.curator.test.TestingServer
 import org.apache.curator.framework.CuratorFrameworkFactory
 
 class ZookeeperConsumerUnitTests
@@ -29,20 +30,23 @@ class ZookeeperConsumerUnitTests
   with GivenWhenThen
   with BeforeAndAfterAll {
 
-  val zookeeperCluster = "localhost:2181"
-  val retryPolicy = new ExponentialBackoffRetry(1000, 3)
+  val zookeeperCluster = "localhost:4711"
+  val retryPolicy = new RetryOneTime(500)
+  val zookeeperServer = new TestingServer(4711)
+  //lazy val zookeeperClient = ZkTestSystem.createZkClient(zookeeperCluster)
   lazy val zookeeperClient = CuratorFrameworkFactory.newClient(zookeeperCluster, retryPolicy)
   zookeeperClient.start()
   lazy val zookeeperConsumer = new ZookeeperConsumer(zookeeperClient)
   val operation = "theOperation"
   val operationFullPath = s"/stratio/streaming/$operation"
 
+
   override def beforeAll() {
     createZookeeperFullPath()
   }
 
   describe("The zookeeper consumer") {
-    ignore("should not throw a TimeOutException when the znode has been created within the timeout") {
+    it("should not throw a TimeOutException when the znode has been created within the timeout") {
       Given("a uniqueId and an operation")
       val uniqueId = UUID.randomUUID().toString
       val fullPath = s"$operationFullPath/$uniqueId"
@@ -53,7 +57,7 @@ class ZookeeperConsumerUnitTests
       Then("the TimeOutException should not be thrown")
     }
 
-    ignore("should throw a TimeOutException when the znode has not been created within the timeout") {
+    it("should throw a TimeOutException when the znode has not been created within the timeout") {
       Given("a uniqueId and an operation")
       val uniqueId = UUID.randomUUID().toString
       val fullPath = s"$operationFullPath/$uniqueId"
@@ -65,7 +69,7 @@ class ZookeeperConsumerUnitTests
       }
     }
 
-    ignore("should pick up the value from the zNode") {
+    it("should pick up the value from the zNode") {
       Given("a zNode with data")
       val uniqueId = UUID.randomUUID().toString
       val fullPath = s"$operationFullPath/$uniqueId"
@@ -77,7 +81,7 @@ class ZookeeperConsumerUnitTests
       data.get should be ("someData")
     }
 
-    ignore("should return None when the zNode does not exist") {
+    it("should return None when the zNode does not exist") {
       Given("a non existing zNode")
       val uniqueId = UUID.randomUUID().toString
       val fullPath = s"$operationFullPath/$uniqueId"
@@ -87,7 +91,7 @@ class ZookeeperConsumerUnitTests
       data.isDefined should be (false)
     }
 
-    ignore("should remove a zNode") {
+    it("should remove a zNode") {
       Given("an existing zNode")
       val uniqueId = UUID.randomUUID().toString
       val fullPath = s"$operationFullPath/$uniqueId"
@@ -98,7 +102,7 @@ class ZookeeperConsumerUnitTests
       theNodeExists(fullPath) should be(false)
     }
 
-    ignore("should not throw an Exception when removing a non existing node") {
+    it("should not throw an Exception when removing a non existing node") {
       Given("a non existing zNode")
       val uniqueId = UUID.randomUUID().toString
       val fullPath = s"$operationFullPath/$uniqueId"
