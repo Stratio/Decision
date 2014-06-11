c0e3229c3bb199c33c19b3bc66dc915627f741c7
Alberto Rodriguez <albertorodriguez@paradigmatecnologico.com>
2014-06-11 18:25:40 +0200

    Add StratioAPIGenericException to list operations

--- shell/src/main/java/com/stratio/streaming/shell/dao/CachedStreamsDAO.java
+++ shell/src/main/java/com/stratio/streaming/shell/dao/CachedStreamsDAO.java
@@ -17,6 +17,7 @@ package com.stratio.streaming.shell.dao;
 
 import java.util.List;
 
+import com.stratio.streaming.commons.exceptions.StratioAPIGenericException;
 import com.stratio.streaming.commons.exceptions.StratioAPISecurityException;
 import com.stratio.streaming.commons.exceptions.StratioEngineOperationException;
 import com.stratio.streaming.commons.exceptions.StratioEngineStatusException;
@@ -25,7 +26,7 @@ import com.stratio.streaming.messaging.ColumnNameType;
 
 public interface CachedStreamsDAO {
 
-    List<StratioStream> listStreams() throws StratioEngineStatusException;
+    List<StratioStream> listStreams() throws StratioEngineStatusException, StratioAPIGenericException;
 
     void newStream(String name, List<ColumnNameType> columns) throws StratioEngineStatusException,
             StratioAPISecurityException, StratioEngineOperationException;
@@ -33,6 +34,6 @@ public interface CachedStreamsDAO {
     void dropStream(String name) throws StratioEngineStatusException, StratioAPISecurityException,
             StratioEngineOperationException;
 
-    List<StratioStream> listUncachedStreams() throws StratioEngineStatusException;
+    List<StratioStream> listUncachedStreams() throws StratioEngineStatusException, StratioAPIGenericException;
 
 }
