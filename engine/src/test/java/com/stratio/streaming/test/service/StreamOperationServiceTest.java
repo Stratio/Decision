2bb26ba2fe1f7a6e2eb89bb3af472b80e7b5eef1
Antonio Jesus Navarro Perez <ajnavarro@paradigmatecnologico.com>
2014-08-11 09:09:14 +0200

    removed unused dependencies

--- engine/src/test/java/com/stratio/streaming/test/service/StreamOperationServiceTest.java
+++ engine/src/test/java/com/stratio/streaming/test/service/StreamOperationServiceTest.java
@@ -19,8 +19,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
-import junit.framework.Assert;
-
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
