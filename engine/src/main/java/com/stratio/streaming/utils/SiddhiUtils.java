9cec8e78593b978e8098f303048d26b5edad0073
Antonio Jesus Navarro Perez <ajnavarro@paradigmatecnologico.com>
2014-08-11 08:59:31 +0200

    removed deprecated methods

--- engine/src/main/java/com/stratio/streaming/utils/SiddhiUtils.java
+++ engine/src/main/java/com/stratio/streaming/utils/SiddhiUtils.java
@@ -15,105 +15,23 @@
  */
 package com.stratio.streaming.utils;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.UUID;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.wso2.siddhi.core.SiddhiManager;
-import org.wso2.siddhi.core.config.SiddhiConfiguration;
-import org.wso2.siddhi.query.api.QueryFactory;
 import org.wso2.siddhi.query.api.definition.Attribute;
-import org.wso2.siddhi.query.api.definition.Attribute.Type;
 import org.wso2.siddhi.query.api.definition.StreamDefinition;
 import org.wso2.siddhi.query.api.exception.AttributeNotExistException;
 
-import com.hazelcast.config.Config;
-import com.hazelcast.config.JoinConfig;
-import com.hazelcast.config.NetworkConfig;
-import com.hazelcast.instance.HazelcastInstanceFactory;
-import com.stratio.streaming.commons.constants.ColumnType;
 import com.stratio.streaming.commons.constants.STREAMING;
 import com.stratio.streaming.commons.constants.STREAM_OPERATIONS;
 import com.stratio.streaming.commons.messages.ColumnNameTypeValue;
-import com.stratio.streaming.commons.messages.StratioStreamingMessage;
-import com.stratio.streaming.extensions.DistinctWindowExtension;
 
 public class SiddhiUtils {
-
-    private static Logger logger = LoggerFactory.getLogger(SiddhiUtils.class);
 
     public static final String QUERY_PLAN_IDENTIFIER = "StratioStreamingCEP-Cluster";
 
     private SiddhiUtils() {
 
-    }
-
-    @Deprecated
-    public static Type decodeSiddhiType(ColumnType originalType) {
-        switch (originalType) {
-        case STRING:
-            return Attribute.Type.STRING;
-        case BOOLEAN:
-            return Attribute.Type.BOOL;
-        case DOUBLE:
-            return Attribute.Type.DOUBLE;
-        case INTEGER:
-            return Attribute.Type.INT;
-        case LONG:
-            return Attribute.Type.LONG;
-        case FLOAT:
-            return Attribute.Type.FLOAT;
-        default:
-            throw new RuntimeException("Unsupported Column type: " + originalType);
-        }
-    }
-
-    public static ColumnType encodeSiddhiType(Type type) {
-        switch (type) {
-        case STRING:
-            return ColumnType.STRING;
-        case BOOL:
-            return ColumnType.BOOLEAN;
-        case DOUBLE:
-            return ColumnType.DOUBLE;
-        case INT:
-            return ColumnType.INTEGER;
-        case LONG:
-            return ColumnType.LONG;
-        case FLOAT:
-            return ColumnType.FLOAT;
-        default:
-            throw new RuntimeException("Unsupported Column type: " + type);
-        }
-    }
-
-    @Deprecated
-    public static String recoverStreamDefinition(StreamDefinition streamDefinition) {
-
-        String attributesList = "";
-
-        for (Attribute field : streamDefinition.getAttributeList()) {
-            attributesList += field.getName() + " " + field.getType().toString().toLowerCase() + ",";
-        }
-
-        return "define stream " + streamDefinition.getStreamId() + "("
-                + attributesList.substring(0, attributesList.length() - 1) + ")";
-    }
-
-    @Deprecated
-    public static StreamDefinition buildDefineStreamSiddhiQL(StratioStreamingMessage request) {
-
-        StreamDefinition newStream = QueryFactory.createStreamDefinition().name(request.getStreamName());
-
-        for (ColumnNameTypeValue column : request.getColumns()) {
-            logger.info(column.getColumn() + "//" + SiddhiUtils.decodeSiddhiType(column.getType()));
-            newStream.attribute(column.getColumn(), SiddhiUtils.decodeSiddhiType(column.getType()));
-        }
-
-        return newStream;
     }
 
     public static boolean columnAlreadyExistsInStream(String columnName, StreamDefinition streamMetaData) {
@@ -125,47 +43,6 @@ public class SiddhiUtils {
         }
 
         return false;
-    }
-
-    /**
-     * 
-     * - Instantiates the Siddhi CEP engine - return the running CEP engine
-     * 
-     * 
-     * @return SiddhiManager
-     */
-    public static SiddhiManager setupSiddhiManager(String cassandraCluster, Boolean failOverEnabled) {
-
-        SiddhiConfiguration conf = new SiddhiConfiguration();
-        conf.setInstanceIdentifier("StratioStreamingCEP-Instance-" + UUID.randomUUID().toString());
-        conf.setQueryPlanIdentifier(QUERY_PLAN_IDENTIFIER);
-        conf.setDistributedProcessing(false);
-
-        @SuppressWarnings("rawtypes")
-        List<Class> extensions = new ArrayList<>();
-        extensions.add(DistinctWindowExtension.class);
-        conf.setSiddhiExtensions(extensions);
-
-        // Create Siddhi Manager
-        SiddhiManager siddhiManager = new SiddhiManager(conf);
-
-        Config config = new Config();
-        config.setInstanceName("stratio-streaming-hazelcast-instance");
-        NetworkConfig network = config.getNetworkConfig();
-        JoinConfig join = network.getJoin();
-        join.getMulticastConfig().setEnabled(false);
-
-        siddhiManager.getSiddhiContext().setHazelcastInstance(HazelcastInstanceFactory.newHazelcastInstance(config));
-
-        // if (failOverEnabled) {
-        //
-        // siddhiManager.setPersistStore(new
-        // Casandra2PersistenceStore(cassandraCluster, "", ""));
-        //
-        // StreamPersistence.restoreLastRevision(siddhiManager);
-        // }
-
-        return siddhiManager;
     }
 
     public static Object[] getOrderedValues(StreamDefinition streamMetaData, List<ColumnNameTypeValue> columns)
