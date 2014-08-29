eb2fea3885feff99d47177877527ff9b39d78280
Antonio Jesus Navarro Perez <ajnavarro@paradigmatecnologico.com>
2014-08-29 12:40:03 +0200

    added examples module.

--- examples/src/main/java/com/stratio/streaming/executables/HardwareEmulatorMain.java
+++ examples/src/main/java/com/stratio/streaming/executables/HardwareEmulatorMain.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.stratio.streaming.examples;
+package com.stratio.streaming.executables;
 
 import java.util.ArrayList;
 import java.util.List;
