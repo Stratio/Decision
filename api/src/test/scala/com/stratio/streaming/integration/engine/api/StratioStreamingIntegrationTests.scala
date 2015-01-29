c4f7a9548b055a002f1f6b9cdc3129225ba57166
ajnavarro-stratio <ajnavarro@stratio.com>
2015-01-29 18:12:45 +0100

    fixed api junit tests execution

--- api/src/test/scala/com/stratio/streaming/integration/engine/api/StratioStreamingIntegrationTests.scala
+++ api/src/test/scala/com/stratio/streaming/integration/engine/api/StratioStreamingIntegrationTests.scala
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.stratio.streaming.integration
+package com.stratio.streaming.integration.engine.api
 
 import com.datastax.driver.core.querybuilder.QueryBuilder
 import com.datastax.driver.core.{Cluster, Session}
