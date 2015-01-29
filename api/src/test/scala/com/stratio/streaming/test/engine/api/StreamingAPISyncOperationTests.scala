611fc8198f342aa7421cf406055a3520ebca58d3
ajnavarro-stratio <ajnavarro@stratio.com>
2015-01-29 17:45:25 +0100

    changed unit tests packages

--- api/src/test/scala/com/stratio/streaming/test/engine/api/StreamingAPISyncOperationTests.scala
+++ api/src/test/scala/com/stratio/streaming/test/engine/api/StreamingAPISyncOperationTests.scala
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.stratio.streaming.unit
+package com.stratio.streaming.test.engine.api
 
 import scala.collection.JavaConversions.seqAsJavaList
 import scala.concurrent.Future
--- api/src/test/scala/com/stratio/streaming/test/engine/api/ZookeeperConsumerUnitTests.scala
+++ api/src/test/scala/com/stratio/streaming/test/engine/api/ZookeeperConsumerUnitTests.scala
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.stratio.streaming.unit
+package com.stratio.streaming.test.engine.api
 
 import org.scalatest.{ BeforeAndAfterAll, GivenWhenThen, ShouldMatchers, FunSpec }
 import scala.concurrent._
