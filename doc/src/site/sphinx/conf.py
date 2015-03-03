01e8cda0ff6f84c25e9f2a07c2c359eed87c0482
witokondoria <witokondoria@gmail.com>
2015-03-03 17:50:00 +0100

    Added submodule with Stratio's sphinx theme

--- doc/src/site/sphinx/conf.py
+++ doc/src/site/sphinx/conf.py
@@ -100,19 +100,16 @@ highlight_language = 'scala'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'bootstrap'
+html_theme = 'stratio'
 
 #html_logo = "logo.gif"
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {
-    'navbar_sidebarrel': True,
-    'navbar_pagenav': False,
-    'globaltoc_depth': 2,
-    'navbar_fixed_top': "false",
-    'source_link_position': "no",
-    'bootswatch_theme': "cosmo",
+html_theme_options = {    
+    'versions' : ['0.7.0-SNAPSHOT'],
+    'github' : 'https://github.com/Stratio/streaming-cep-engine',
+    'jira': 'https://stratio.atlassian.net/secure/Dashboard.jspa'
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
