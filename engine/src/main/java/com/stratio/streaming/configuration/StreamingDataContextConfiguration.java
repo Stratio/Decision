83130b8c11bf9f75c484dd489b877325d245e3fe
Mario Muñoz <mmunoz@paradigmatecnologico.com>
2015-07-03 13:35:02 +0200

    Back to separated Spark Contexts in several JVMs.

--- engine/src/main/java/com/stratio/streaming/configuration/StreamingDataContextConfiguration.java
+++ engine/src/main/java/com/stratio/streaming/configuration/StreamingDataContextConfiguration.java
@@ -1,63 +1,3 @@
-<<<<<<< HEAD
-//package com.stratio.streaming.configuration;
-//
-//import com.stratio.streaming.commons.constants.BUS;
-//import com.stratio.streaming.commons.constants.InternalTopic;
-//import com.stratio.streaming.commons.constants.STREAM_OPERATIONS;
-//import com.stratio.streaming.commons.messages.StratioStreamingMessage;
-//import com.stratio.streaming.functions.dml.InsertIntoStreamFunction;
-//import com.stratio.streaming.functions.messages.FilterMessagesByOperationFunction;
-//import com.stratio.streaming.functions.messages.KeepPayloadFromMessageFunction;
-//import org.apache.spark.streaming.api.java.JavaDStream;
-//import org.apache.spark.streaming.api.java.JavaPairDStream;
-//import org.apache.spark.streaming.api.java.JavaStreamingContext;
-//import org.apache.spark.streaming.kafka.KafkaUtils;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.annotation.Import;
-//import org.springframework.context.annotation.Lazy;
-//
-//import java.util.HashMap;
-//import java.util.Map;
-//
-//@Configuration
-//@Import(ServiceConfiguration.class)
-//public class StreamingDataContextConfiguration extends StreamingContextConfiguration {
-//
-//    @Bean(name = "dataContext", destroyMethod = "stop")
-//    @Lazy
-//    public JavaStreamingContext dataContext() {
-//        JavaStreamingContext context = this.create("stratio-streaming-data", 4041,
-//                configurationContext.getInternalStreamingBatchTime(), configurationContext.getInternalSparkHost());
-//
-//        KeepPayloadFromMessageFunction keepPayloadFromMessageFunction = new KeepPayloadFromMessageFunction();
-//
-//        Map<String, Integer> baseTopicMap = new HashMap<String, Integer>();
-//        baseTopicMap.put(InternalTopic.TOPIC_DATA.getTopicName(), 1);
-//
-//        kafkaTopicService.createTopicIfNotExist(InternalTopic.TOPIC_DATA.getTopicName(),
-//                configurationContext.getKafkaReplicationFactor(), configurationContext.getKafkaPartitions());
-//
-//        JavaPairDStream<String, String> messages = KafkaUtils.createStream(context,
-//                configurationContext.getZookeeperHostsQuorum(), BUS.STREAMING_GROUP_ID, baseTopicMap);
-//
-//        messages.cache();
-//
-//        JavaDStream<StratioStreamingMessage> insertRequests = messages.filter(
-//                new FilterMessagesByOperationFunction(STREAM_OPERATIONS.MANIPULATION.INSERT)).map(
-//                keepPayloadFromMessageFunction);
-//
-//        InsertIntoStreamFunction insertIntoStreamFunction = new InsertIntoStreamFunction(streamOperationService,
-//                configurationContext.getZookeeperHostsQuorum());
-//
-//        insertRequests.foreachRDD(insertIntoStreamFunction);
-//
-//        return context;
-//
-//    }
-//
-//}
-=======
 package com.stratio.streaming.configuration;
 
 import com.stratio.streaming.commons.constants.BUS;
@@ -116,4 +56,3 @@ public class StreamingDataContextConfiguration extends StreamingContextConfigura
     }
 
 }
->>>>>>> master
