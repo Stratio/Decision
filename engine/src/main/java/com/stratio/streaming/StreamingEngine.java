4adc138c717ea233dc08aafc0323fad00fc0aab8
Mario Muñoz <mmunoz@paradigmatecnologico.com>
2015-08-06 09:47:06 +0200

    Merge branch 'feature/high-availability'

--- engine/src/main/java/com/stratio/streaming/StreamingEngine.java
+++ engine/src/main/java/com/stratio/streaming/StreamingEngine.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *         http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -44,11 +44,11 @@ public class StreamingEngine {
                         BaseConfiguration.class)) {
                     annotationConfigApplicationContext.registerShutdownHook();
 
-                   JavaStreamingContext context = annotationConfigApplicationContext.getBean("streamingContext", JavaStreamingContext.class);
+                    JavaStreamingContext context = annotationConfigApplicationContext.getBean("streamingContext", JavaStreamingContext.class);
 
-            context.start();
+                    context.start();
 
-            context.awaitTermination();
+                    context.awaitTermination();
                 } catch (Exception e) {
                     log.error("Fatal error", e);
                 }
