0b28bc15acb52a5af078448cfef4a966f7bf7b7a
Aitor <aargomaniz@stratio.com>
2015-09-21 13:10:52 +0200

    Implements CEP Siddhi test suite and adds Orders demo queries validation

---
+++ siddhi/src/test/java/com/stratio/streaming/unit/siddhi/query/SiddhiQueryHelper.java
@@ -0,0 +1,35 @@
+package com.stratio.streaming.unit.siddhi.query;
+
+import com.stratio.streaming.siddhi.extension.DistinctWindowExtension;
+import org.wso2.siddhi.core.SiddhiManager;
+import org.wso2.siddhi.core.config.SiddhiConfiguration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Created by aitor on 9/16/15.
+ */
+public abstract class SiddhiQueryHelper {
+
+    public static SiddhiManager sm;
+
+    public static AtomicInteger count;
+
+    public static String STREAM_NAME= "streamBase";
+    public static String STREAM_RESULT= "resultStream";
+
+    public static SiddhiManager initializeSiddhiManager() {
+        SiddhiConfiguration config = new SiddhiConfiguration();
+        @SuppressWarnings("rawtypes")
+        List<Class> extensions = new ArrayList<Class>();
+        extensions.add(DistinctWindowExtension.class);
+        config.setSiddhiExtensions(extensions);
+        sm = new SiddhiManager(config);
+        return sm;
+    }
+
+    public static void shutdownSiddhiManager()    {
+        sm.shutdown();
+    }
+}
