ce0ce69a297df319d9de77590ea4b73db60d28b2
Blinx7 <becaresss@gmail.com>
2015-10-26 17:02:10 +0100

    testAT added

---
+++ testAT/src/test/java/com/stratio/decision/testsAT/specs/Common.java
@@ -0,0 +1,7 @@
+package com.stratio.decision.testsAT.specs;
+
+import com.stratio.specs.CommonG;
+
+public class Common extends CommonG {
+
+}
---
+++ testAT/src/test/java/com/stratio/decision/testsAT/specs/WhenSpec.java
@@ -0,0 +1,9 @@
+package com.stratio.decision.testsAT.specs;
+
+public class WhenSpec extends BaseSpec {
+
+    public WhenSpec(Common spec) {
+        this.commonspec = spec;
+    }
+
+}
